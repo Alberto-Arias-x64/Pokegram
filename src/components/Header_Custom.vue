2b70bcb053f9deb490e880d7ebea6218eca5de21
Alberto_x64 <alberto.arias.x64@gmail.com>
2022-08-15 20:21:13 -0500

    saving de word

---
+++ src/components/Header_Custom.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Pija Paja</h1>
+        <input type="text">
+        <ion-icon name="home-outline"></ion-icon>
+        <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+        <ion-icon name="add-circle-outline"></ion-icon>
+        <ion-icon name="compass-outline"></ion-icon>
+        <ion-icon name="add-circle-outline"></ion-icon>
+    </div>
+</template>
+<style scoped>
+    div{
+        background-color: white;
+        color: black;
+        display: flex;
+        flex-direction: row ;
+    }
+</style>
