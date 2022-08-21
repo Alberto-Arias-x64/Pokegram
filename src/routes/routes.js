1ce0404a41ce5925e4a893caf2dce8c20e625b0c
Alberto_x64 <alberto.arias.x64@gmail.com>
2022-08-20 22:51:22 -0500

    hash routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {path:'/home', component: Home} , // Carga sincrona (mas lenta pero siempre disponible)
         {path:'/chat', component: () => import('../views/Home.vue')}, // Carga asincrona (mas rapido la carga pero esta la solicita y no sobrecarga el inicio)
