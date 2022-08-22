c5bf96fc6b5c8cea698f42cd0eecb0f6f3e11a24
Alberto_x64 <alberto.arias.x64@gmail.com>
2022-08-22 13:27:13 -0500

    ready interseption

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ import No_Found from '../views/No_Found.vue'
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
+        {path:'/', redirect:'/home'},
         {path:'/home', component: Home} , // Carga sincrona (mas lenta pero siempre disponible)
          // Carga asincrona (mas rapido la carga pero esta la solicita y no sobrecarga el inicio) () => import('../views/Home.vue')
         {path:'/:catchAll(.*)', component: No_Found }
