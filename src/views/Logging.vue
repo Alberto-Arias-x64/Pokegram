e643ec86cac12039d668267300d1f817f2cceef8
Alberto_x64 <alberto.arias.x64@gmail.com>
2022-08-30 21:50:48 -0500

    adding login page

---
+++ src/views/Logging.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import {watch,} from 'vue'
+    
+    const New_User = 'pikahu'
+    const New_Password = '1234'
+
+</script>
+
+<template>
+    <h2>Logging to pokegram</h2>
+    <input type="text" placeholder="User" v-model="New_User">
+    <input type="text" placeholder="Password" v-model="New_Password">
+    <p>{{New_User}}</p>
+</template>
+
+<style scoped>
+    h2,p{
+        text-align: center;
+    }
+</style>
